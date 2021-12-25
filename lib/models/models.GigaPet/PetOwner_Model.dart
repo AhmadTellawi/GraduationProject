0ae456fdbf456bbad17727926f430954f6506191
Monte <montaser704@gmail.com>
2021-12-25 18:55:28 +0200

    MOOOOOOOOOOOOOOOOOOOOOOOOOOOOONte

--- lib/models/models.GigaPet/PetOwner_Model.dart
+++ lib/models/models.GigaPet/PetOwner_Model.dart
@@ -3,9 +3,7 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
-class PetOwnerModel
-{
-
+class PetOwnerModel {
   late String fName;
   late String lName;
   late String Username;
@@ -18,25 +16,25 @@ class PetOwnerModel
     this.lName,
     this.Username,
     this.uID,
-      );
+  );
 
-  PetOwnerModel.fromJson(Map<String, dynamic>json){
-  email = json['email'];
-  fName = json['fName'];
-  lName = json['lName'];
-  Username = json['username'];
-  uID = json['uID'];
+  factory PetOwnerModel.fromMap(Map<String, dynamic> data, String personId) {
+    final String email = data['email'];
+    final String fName = data['fName'];
+    final String lName = data['lName'];
+    final String Username = data['Username'];
+    final String uID = data['uID'];
+
+    return PetOwnerModel(email, fName, lName, Username, uID);
   }
 
-
-  Map<String, dynamic> toMap()
-  {
-   return {
-     'fname': fName,
-     'lname': lName,
-     'email': email,
-     'Username':Username,
-     'uID': uID,
-   };
+  Map<String, dynamic> toMap() {
+    return {
+      'fname': fName,
+      'lname': lName,
+      'email': email,
+      'Username': Username,
+      'uID': uID,
+    };
   }
 }
